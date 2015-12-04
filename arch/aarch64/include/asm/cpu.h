d16746c76b3dfe58e96a03375c6887f3c82333d7
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2015-12-04 17:39:07 +0000

    AArch64: factor CPU ID getters
    This patch adds a simple utility to read the current CPU ID from C or
    assembly. The underlying objective of this seemingly useless change is
    to provide a clean base for C helpers. It introduces two include paths:
    include/ and arch/*/include/asm
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>

---
+++ arch/aarch64/include/asm/cpu.h
@@ -0,0 +1,27 @@
+/*
+ * arch/aarch64/include/asm/cpu.h
+ *
+ * Copyright (C) 2015 ARM Limited. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE.txt file.
+ */
+#ifndef __ASM_AARCH64_CPU_H
+#define __ASM_AARCH64_CPU_H
+
+#define MPIDR_ID_BITS		0xff00ffffff
+
+#ifndef __ASSEMBLY__
+
+static inline unsigned long read_mpidr(void)
+{
+	unsigned long mpidr;
+
+	asm volatile ("mrs	%0, mpidr_el1\n" : "=r" (mpidr));
+	return mpidr & MPIDR_ID_BITS;
+}
+
+
+#endif /* !__ASSEMBLY__ */
+
+#endif
---
+++ include/cpu.h
@@ -0,0 +1,16 @@
+/*
+ * include/cpu.h - Generic CPU features
+ *
+ * Copyright (C) 2015 ARM Limited. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE.txt file.
+ */
+#ifndef __CPU_H
+#define __CPU_H
+
+#include <asm/cpu.h>
+
+#define MPIDR_INVALID	(-1)
+
+#endif
