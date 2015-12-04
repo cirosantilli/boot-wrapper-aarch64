fc4857f4292d0dfc40b5c32d80f16d47e27f98cc
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2015-12-04 16:42:42 +0000

    AArch64: clean common.S
    Remove macro drop_el since its only user is now boot.S, and move all
    defines to cpu.h.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>

--- arch/aarch64/include/asm/cpu.h
+++ arch/aarch64/include/asm/cpu.h
@@ -11,6 +11,23 @@
 
 #define MPIDR_ID_BITS		0xff00ffffff
 
+#define CURRENTEL_EL3		(3 << 2)
+
+/*
+ * RES1 bits,  little-endian, caches and MMU off, no alignment checking,
+ * no WXN.
+ */
+#define SCTLR_EL2_RESET		(3 << 28 | 3 << 22 | 1 << 18 | 1 << 16 | 1 << 11 | 3 << 4)
+
+#define SPSR_A			(1 << 8)	/* System Error masked */
+#define SPSR_D			(1 << 9)	/* Debug masked */
+#define SPSR_I			(1 << 7)	/* IRQ masked */
+#define SPSR_F			(1 << 6)	/* FIQ masked */
+#define SPSR_T			(1 << 5)	/* Thumb */
+#define SPSR_EL2H		(9 << 0)	/* EL2 Handler mode */
+
+#define SPSR_KERNEL		(SPSR_A | SPSR_D | SPSR_I | SPSR_F | SPSR_EL2H)
+
 #ifndef __ASSEMBLY__
 
 #define sevl()		asm volatile ("sevl\n" : : : "memory")
